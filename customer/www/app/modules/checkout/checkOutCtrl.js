8d4c2e7fbb2dfb34f02039294ec7b2653b96a01a
kamaldlk <kamalcdsk@gmail.com>
2015-10-25 16:24:16 +0530

    checkout pages

--- customer/www/app/modules/checkout/checkOutCtrl.js
+++ customer/www/app/modules/checkout/checkOutCtrl.js
@@ -41,7 +41,7 @@ angular.module('aviate.controllers')
                 id: Math.random(),
                 firstName: "saravanan",
                 lastName: "R",
-                address: "chennai",
+                address:{line1:"jkhjkhk",line2:"jhgjhg"},
                 city: "chennai",
                 state: "TN",
                 country: "IN",
@@ -55,100 +55,6 @@ angular.module('aviate.controllers')
                 payment: false
             };
              
-             
-
-
-			$scope.addAddress = function(address){
-				CheckOutServices.addAddress(address).then(function(data){
-
-				});
-			};
-
-			$scope.getAddressList = function(address){
-				CheckOutServices.getAddressList(address).then(function(data){
-
-				});
-			};
-
-			$scope.removeAddress = function(address){
-				CheckOutServices.removeAddress(address).then(function(data){
-
-				});
-			};
-
-
-			$scope.confirmOrder = function(){
-
-				var order = angular.toJson({
-					"customerId":$rootScope.user.userId,
-					"addressId":$scope.deliveryAddress.addressId,
-					"storeId":$rootScope.store.storeId,
-					"deliveryDate":$scope.deliveryDate,
-					"deliveryTime":$rootScope.deliveryTime,
-					"contactNo":$rootScope.contactNo,
-					"totalAmount":$rootScope.mycart.cartTotalAmount,
-					"orderGrossAmount":$rootScope.mycart.grossAmount,
-					"totalTaxAmount":$rootScope.mycart.taxAmount,
-					"shippingCharge":$rootScope.mycart.shippingCharges
-				});
-
-				CheckOutServices.confirmOrder(order).then(function(data){
-					$scope.payment();
-				});
-
-				/*
-
-				$http({
-					url: serviceUrl + 'aviate/json/checkout/conformorder',
-					method: 'POST',
-					data: menuJson,
-					headers: {
-						'Content-Type': 'application/json'
-					} 
-				}).success(function(result, status, headers){
-					$scope.orderId = result.orderId;
-					$scope.$apply();
-					$scope.payment();
-				})
-				 */}
-
-
-			$scope.payment = function() {
-
-				CheckOutServices.payment(order).then(function(data){
-
-					$scope.timeStamp = data.timestamp;
-					$scope.hash = data.hashMessage;
-					$scope.TOTAL = $rootScope.mycart.grossAmount;
-					$scope.shipping = ($rootScope.mycart.cartTotalAmount);
-					$scope.$apply();
-					document.forms["FirstData"].submit();
-				});
-
-			};
-
-				/*			$scope.addresslist =[];
-				var paymentJSON = angular.toJson
-				({
-					"amount":$rootScope.grossAmount
-				});
-				$http({
-					url: "http://182.74.202.178:8181/aviate/json/get/payment",
-					method: 'POST',
-					data : paymentJSON,
-					headers: {
-						'Content-Type': 'application/json'
-					}
-				}).success(function(result, status, headers){
-					$scope.timeStamp = result.timestamp;
-					$scope.hash = result.hashMessage;
-					$scope.TOTAL = $rootScope.grossAmount;
-					$scope.shipping = ($scope.cartTotalAmount * .05);
-					$scope.$apply();
-					document.forms["FirstData"].submit();
-				});
-			}*/
-        
               $scope.selectAddress = function(addressId) {
                 var seletecdAddress = _.filter($scope.addresses, function(add) {
                     return add.selected == true;
@@ -241,9 +147,4 @@ angular.module('aviate.controllers')
 
                 }
             };
-
-        
-             
-             
-
 		}]);
