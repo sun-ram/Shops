934c8fd0a8795420fb1292e178acb6fa75b0db62
PrabakaranA <prabakaran.a@mitosistech.com>
2015-10-30 15:27:47 +0530

    added in Constant file.

--- customer/www/app/constants/constants.js
+++ customer/www/app/constants/constants.js
@@ -3,14 +3,21 @@ angular.module('aviate.constant').constant('CONSTANT', {
 
 		STATUS:{
 			SUCCESS:'SUCCESS',
-			FAILURE:'FAILURE'
+			FAILURE:'FAILURE',
 		},
 
 		SUCCESS_CODE:{
 			SIGNINSUCCESS: 'Sign in successfully',
 			SIGNUPSUCCESS: 'Sign up successfully',
 			FORGETPASSWORDCONFIRMATION: 'Password has been sent to your mail',
-			ROLE: 'CUSTOMER'
+			ROLE: 'CUSTOMER',
+				ADDPRODUCT:'Product Added Successfully',
+				REMOVEPRODUCT:'Product Removed Successfully',
+				INCREMENTPRODUCT:'Product Increased Successfully',
+				DECREASEPRODUCT:'Product Decreased Successfully',
+				ADDADDRESS:'Address Added Successfully',
+				UPDATEADDRESS:'Address Updated Successfully',
+				REMOVEADDRESS:'Address Removed Successfully'				
 		},
 
 		ERROR_CODE:{
