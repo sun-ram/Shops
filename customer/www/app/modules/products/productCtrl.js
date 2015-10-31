b4fe30cde5136997dce852c8b8d90eb8e18f5a67
PrabakaranA <prabakaran.a@mitosistech.com>
2015-10-31 17:35:20 +0530

    isuues fixed.

--- customer/www/app/modules/products/productCtrl.js
+++ customer/www/app/modules/products/productCtrl.js
@@ -1,31 +1,29 @@
 angular.module('aviate.controllers')
 .controller("productCtrl",
-		['$scope', '$state', 'toastr', 'CONSTANT', 'ProductService','products', '$rootScope', 'ipCookie', 'MyCartFactory', 'MyCartServices','$mdDialog',
-		 function($scope, $state, toastr, CONSTANT, ProductService, products,$rootScope,ipCookie, MyCartFactory, MyCartServices,$mdDialog) {
+		['$scope', '$state', 'toastr', 'CONSTANT', 'ProductService','products', '$rootScope', 'ipCookie', 'MyCartFactory', 'MyCartServices','$mdDialog','MyListServices',
+		 function($scope, $state, toastr, CONSTANT, ProductService, products,$rootScope,ipCookie, MyCartFactory, MyCartServices,$mdDialog,MyListServices) {
 
 
 			$scope.productList = products;
-
-//			$rootScope.addToCart = function(product){
-//				MyCartFactory.addToCart(product,$scope.productList,  function(data){
-//					$scope.productList = data;
-//					$scope.getCartList();
-//				});
-//			}
-//
-//			$scope.getCartList = function(){
-//				if($rootScope.user){
-//					MyCartServices.getCartList({"customerId" : $rootScope.user.userId, "storeId" : $rootScope.store.storeId},  function(data){
-//						MyCartFactory.checkCartProductsQuantity($scope.productList,function(data){
-//							$scope.productList = data;
-//						});
-//					});
-//				}
-//			}
-
-/*			$scope.productDetails = function(products){
-				$state.go('app.productsdetails',{productId:products.productId});
-			}*/
+			
+			$scope.addToMyList = function(product){
+				if($rootScope.user == null || $rootScope.user == undefined){
+					$rootScope.signInPopup();
+				}else{
+				MyListServices.addToMyList({"customerId":$rootScope.user.userId,"productId" : product.productId,"storeId" : $rootScope.store.storeId}).then(function(data){
+					$scope.productDetails.isProductMyList = true;
+					$scope.getMyList();
+				});	
+				}
+			};
+			
+			$scope.getMyList = function(){
+				MyListServices.getMyList({"customerId":$rootScope.user.userId, "storeId" : $rootScope.store.storeId}).then(function(data){
+					$scope.myListProducts = data;
+				});
+			};
+			
+			
 			$scope.productDetails = function(ev,products){
 				$rootScope.productDetails = products;
 				$mdDialog.show({
