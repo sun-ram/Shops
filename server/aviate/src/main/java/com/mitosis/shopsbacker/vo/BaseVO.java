c7e499889fcf4dcd12b099527ce3239765751eb6
Sundaram <sundaram.c@mitosistech.com>
2015-11-11 14:19:48 +0530

    Initial Commitment

---
+++ server/aviate/src/main/java/com/mitosis/shopsbacker/vo/BaseVO.java
@@ -0,0 +1,126 @@
+package com.mitosis.shopsbacker.vo;
+
+import java.util.Date;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class BaseVO {
+	private String createUser;
+
+	private Date createDate;
+
+	private String lastUpdateUser;
+
+	private Date lastUpdateDate;
+
+	private String errorCode;
+	private String errorMessage;
+	private String status;
+
+	/**
+	 * @return the createUser
+	 */
+	public String getCreateUser() {
+		return createUser;
+	}
+
+	/**
+	 * @param createUser
+	 *            the createUser to set
+	 */
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+
+	/**
+	 * @return the createDate
+	 */
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	/**
+	 * @param createDate
+	 *            the createDate to set
+	 */
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	/**
+	 * @return the lastUpdateUser
+	 */
+	public String getLastUpdateUser() {
+		return lastUpdateUser;
+	}
+
+	/**
+	 * @param lastUpdateUser
+	 *            the lastUpdateUser to set
+	 */
+	public void setLastUpdateUser(String lastUpdateUser) {
+		this.lastUpdateUser = lastUpdateUser;
+	}
+
+	/**
+	 * @return the lastUpdateDate
+	 */
+	public Date getLastUpdateDate() {
+		return lastUpdateDate;
+	}
+
+	/**
+	 * @param lastUpdateDate
+	 *            the lastUpdateDate to set
+	 */
+	public void setLastUpdateDate(Date lastUpdateDate) {
+		this.lastUpdateDate = lastUpdateDate;
+	}
+
+	/**
+	 * @return the errorCode
+	 */
+	public String getErrorCode() {
+		return errorCode;
+	}
+
+	/**
+	 * @param errorCode
+	 *            the errorCode to set
+	 */
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	/**
+	 * @return the errorMessage
+	 */
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	/**
+	 * @param errorMessage
+	 *            the errorMessage to set
+	 */
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+	
+	
+	
+
+}
