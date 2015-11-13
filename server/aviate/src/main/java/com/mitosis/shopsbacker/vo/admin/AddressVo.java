9779dcadd4748bc446dba1e6dc2c2446dfc84aa2
Sundaram <sundaram.c@mitosistech.com>
2015-11-13 21:32:21 +0530

    Modified

--- server/aviate/src/main/java/com/mitosis/shopsbacker/vo/admin/AddressVo.java
+++ server/aviate/src/main/java/com/mitosis/shopsbacker/vo/admin/AddressVo.java
@@ -1,4 +1,5 @@
 package com.mitosis.shopsbacker.vo.admin;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -19,8 +20,7 @@ import com.mitosis.shopsbacker.model.User;
 public class AddressVo {
 	private String addressId;
 	private State state;
-	private Customer customer;
-	private Country country;
+	private String countryId;
 	private String landmark;
 	private String address1;
 	private String address2;
@@ -29,134 +29,93 @@ public class AddressVo {
 	private String phoneNo;
 	private String latitude;
 	private String longitude;
-	private Character isactive;
-	private String createdby;
-	private String updatedby;
-	private Date created;
-	private Date updated;
-	private List<SalesOrder> salesOrders = new ArrayList<SalesOrder>();
-	private List<User>  users =new ArrayList<User>();
-	private List<MerchantRequest>  merchantRequests = new ArrayList<MerchantRequest>();
+
 	public String getAddressId() {
 		return addressId;
 	}
+
 	public void setAddressId(String addressId) {
 		this.addressId = addressId;
 	}
+
 	public State getState() {
 		return state;
 	}
+
 	public void setState(State state) {
 		this.state = state;
 	}
-	public Customer getCustomer() {
-		return customer;
+
+	public String getCountryId() {
+		return countryId;
 	}
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
+
+	public void setCountryId(String countryId) {
+		this.countryId = countryId;
 	}
-	public Country getCountry() {
-		return country;
-	}
-	public void setCountry(Country country) {
-		this.country = country;
-	}
+
 	public String getLandmark() {
 		return landmark;
 	}
+
 	public void setLandmark(String landmark) {
 		this.landmark = landmark;
 	}
+
 	public String getAddress1() {
 		return address1;
 	}
+
 	public void setAddress1(String address1) {
 		this.address1 = address1;
 	}
+
 	public String getAddress2() {
 		return address2;
 	}
+
 	public void setAddress2(String address2) {
 		this.address2 = address2;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public String getPinCode() {
 		return pinCode;
 	}
+
 	public void setPinCode(String pinCode) {
 		this.pinCode = pinCode;
 	}
+
 	public String getPhoneNo() {
 		return phoneNo;
 	}
+
 	public void setPhoneNo(String phoneNo) {
 		this.phoneNo = phoneNo;
 	}
+
 	public String getLatitude() {
 		return latitude;
 	}
+
 	public void setLatitude(String latitude) {
 		this.latitude = latitude;
 	}
+
 	public String getLongitude() {
 		return longitude;
 	}
+
 	public void setLongitude(String longitude) {
 		this.longitude = longitude;
 	}
-	public Character getIsactive() {
-		return isactive;
-	}
-	public void setIsactive(Character isactive) {
-		this.isactive = isactive;
-	}
-	public String getCreatedby() {
-		return createdby;
-	}
-	public void setCreatedby(String createdby) {
-		this.createdby = createdby;
-	}
-	public String getUpdatedby() {
-		return updatedby;
-	}
-	public void setUpdatedby(String updatedby) {
-		this.updatedby = updatedby;
-	}
-	public Date getCreated() {
-		return created;
-	}
-	public void setCreated(Date created) {
-		this.created = created;
-	}
-	public Date getUpdated() {
-		return updated;
-	}
-	public void setUpdated(Date updated) {
-		this.updated = updated;
-	}
-	public List<SalesOrder> getSalesOrders() {
-		return salesOrders;
-	}
-	public void setSalesOrders(List<SalesOrder> salesOrders) {
-		this.salesOrders = salesOrders;
-	}
-	public List<User> getUsers() {
-		return users;
-	}
-	public void setUsers(List<User> users) {
-		this.users = users;
-	}
-	public List<MerchantRequest> getMerchantRequests() {
-		return merchantRequests;
-	}
-	public void setMerchantRequests(List<MerchantRequest> merchantRequests) {
-		this.merchantRequests = merchantRequests;
-	}
 
-	
 }
