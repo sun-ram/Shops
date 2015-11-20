147d4f631885ef4124bd3ceadebd4997b50ea1f7
fayaz <fayaz@fayaz-desktop>
2015-11-20 15:20:30 +0530

    Commit for database script changes...

--- server/aviate/dbdump/changes/NeedToBeExecute.sql
+++ server/aviate/dbdump/changes/NeedToBeExecute.sql
@@ -1,5 +1,15 @@
 --
 -- DATE  --------       Created By   --------------   Description --------- 
---ex: 
--- 20/11/2015 -----------  Prabakaran  -----------  Initian build Deployment ------- 
+-- 
+-- 20/11/2015 -----------  Fayaz  -----------  ProductyType store null changes ------- 
 --
+ALTER TABLE `shopsbacker`.`product_type` 
+DROP FOREIGN KEY `PROD_TYPE_STORE`;
+ALTER TABLE `shopsbacker`.`product_type` 
+CHANGE COLUMN `STORE_ID` `STORE_ID` VARCHAR(32) NULL ;
+ALTER TABLE `shopsbacker`.`product_type` 
+ADD CONSTRAINT `PROD_TYPE_STORE`
+  FOREIGN KEY (`STORE_ID`)
+  REFERENCES `shopsbacker`.`store` (`STORE_ID`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
