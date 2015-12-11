66963d0be3fa9a4de2b3ea5bf73bbbdfe12689d3
rajamohamedbarook <rajamohamed.b@mitosistech.com>
2015-12-11 19:42:40 +0530

    Grouped bar chart updates

--- admin/www/app/modules/dash_board/super_admin/superAdmin.js
+++ admin/www/app/modules/dash_board/super_admin/superAdmin.js
@@ -6,18 +6,21 @@ aviateAdmin.controller("superDashboardCtrl", ['$scope', '$localStorage', '$locat
 		var width = 300;
 		var height = 300;
 		var millisecondsPerday = 86400000;
+		var deliveryTimeSpan = 3600000;
+		var trafficTimeSpan = 900000;
 		var today = new Date();
 		var todayDateObj = new Date(today.toISOString().substring(0, 10));
 		var yesterdayDateObj = new Date(today.toISOString().substring(0, 10));
 		yesterdayDateObj.setTime(todayDateObj.getTime() - millisecondsPerday);
 		$scope.salesOrders = {Books:[]};
-		$scope.merchants;
+		$scope.merchants = [];
 		$scope.stores;
 		$scope.salesOrderLines;
 		$scope.yesterdayTotSale = 0;
 		$scope.todayTotSale = 0;
 		$scope.totalMerchants = 0;
 		$scope.salesGrowthToday = 0;
+		$scope.qualirtStatsRecords = [];
 
 		$scope.historicalBarChart = [
 			{
@@ -168,6 +171,159 @@ aviateAdmin.controller("superDashboardCtrl", ['$scope', '$localStorage', '$locat
 				return chart;
 			});
 		}
+		
+		function groupedBarChart (){
+				var n = 3, // number of layers
+				m = $scope.merchants.Books.length, // number of samples per layer
+				stack = d3.layout.stack(),
+				layers = stack(d3.range(n).map(function() { return bumpLayer(m, .1); })),
+				yGroupMax = d3.max(layers, function(layer) { return d3.max(layer, function(d) { return d.y; }); }),
+				yStackMax = d3.max(layers, function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); });
+
+			var margin = {top: 40, right: 10, bottom: 20, left: 10},
+				width = 960 - margin.left - margin.right,
+				height = 500 - margin.top - margin.bottom;
+
+			var x = d3.scale.ordinal()
+				.domain(d3.range(m))
+				.rangeRoundBands([0, width], .08);
+
+			var y = d3.scale.linear()
+				.domain([0, yStackMax])
+				.range([height, 0]);
+
+			var color = d3.scale.linear()
+				.domain([0, n - 1])
+				.range(["#ff0000", "#32CD32"]);
+
+			var xAxis = d3.svg.axis()
+				.scale(x)
+				.tickSize(0)
+				.tickPadding(6)
+				.orient("bottom");
+
+			var svg = d3.select("#grouperBarChart").append("svg")
+				.attr("width", width + margin.left + margin.right)
+				.attr("height", height + margin.top + margin.bottom)
+			  .append("g")
+				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+			var layer = svg.selectAll(".layer")
+				.data(layers)
+			  .enter().append("g")
+				.attr("class", "layer")
+				.style("fill", function(d, i) { return color(i); });
+
+			var rect = layer.selectAll("rect")
+				.data(function(d) { return d; })
+			  .enter().append("rect")
+				.attr("x", function(d) { return x(d.x); })
+				.attr("y", height)
+				.attr("width", x.rangeBand())
+				.attr("height", 0);
+
+			rect.transition()
+				.delay(function(d, i) { return i * 10; })
+				.attr("y", function(d) { return y(d.y0 + d.y); })
+				.attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); });
+
+			svg.append("g")
+				.attr("class", "x axis")
+				.attr("transform", "translate(0," + height + ")")
+				.call(xAxis);
+
+			d3.selectAll("input").on("change", change);
+
+			var timeout = setTimeout(function() {
+			  d3.select("input[value=\"grouped\"]").property("checked", true).each(change);
+			}, 2000);
+
+			function change() {
+			  clearTimeout(timeout);
+			  if (this.value === "grouped") transitionGrouped();
+			  else transitionStacked();
+			}
+
+			function transitionGrouped() {
+			  y.domain([0, yGroupMax]);
+
+			  rect.transition()
+				  .duration(500)
+				  .delay(function(d, i) { return i * 10; })
+				  .attr("x", function(d, i, j) { return x(d.x) + x.rangeBand() / n * j; })
+				  .attr("width", x.rangeBand() / n)
+				.transition()
+				  .attr("y", function(d) { return y(d.y); })
+				  .attr("height", function(d) { return height - y(d.y); });
+			}
+
+			function transitionStacked() {
+			  y.domain([0, yStackMax]);
+
+			  rect.transition()
+				  .duration(500)
+				  .delay(function(d, i) { return i * 10; })
+				  .attr("y", function(d) { return y(d.y0 + d.y); })
+				  .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); })
+				.transition()
+				  .attr("x", function(d) { return x(d.x); })
+				  .attr("width", x.rangeBand());
+			}
+
+			// Inspired by Lee Byron's test data generator.
+			getQualityStats();
+			function bumpLayer(n, o) {
+
+			  function bump(a) {
+				  
+				  console.log("a--->");
+				  
+				var x = 5,
+					y = 5,
+					z = 5;
+				  console.log("X,Y,Z ==>",x,'&',y,'&',z);
+				for (var i = 0; i < n; i++) {
+				  var w = (i / n - y) * z;
+				  a[i] += x * Math.exp(-w * w);
+				}
+			  }
+
+			  var a = [], i;
+			  for (i = 0; i < n; ++i) a[i] = 1;
+			  for (i = 0; i < 5; ++i) bump(a);
+			  return a.map(function(d, i) { return {x: i, y: Math.max(0, d)}; });
+			}
+		};
+		
+		function getQualityStats (){
+			var i=0,len=$scope.merchants.length;
+			var tmpArray = [],red,green,yellow;
+			var createdTime;
+			var deliveredTime; 
+			for(;i<len;i++){
+				tmpArray = [];
+				red=green=yellow=0;
+				var j=0;soLen = $scope.salesOrders.Books.length;
+				for(;j<solen;j++){
+					if($scope.salesOrders.Books[j].MERCHANT_ID == $scope.merchants.Books[i].MERCHANT_ID && $scope.salesOrders.Books[j].DELIVERY_DATE){
+						createdTime = new Date($scope.salesOrders.Books[j].CREATED);
+						deliveredTime = new Date($scope.salesOrders.Books[j].DELIVERY_DATE);
+						if((deliveredTime.getTime() - createdTime.getTime()) <= deliveryTimeSpan){
+							green++;
+						}else if((deliveredTime.getTime() - createdTime.getTime()) <= (deliveryTimeSpan + trafficTimeSpan) ){
+							yellow++;
+						}else{
+							red++;
+						}
+					}
+				}
+				tmpArray.push(green);
+				tmpArray.push(yellow);
+				tmpArray.push(red);	
+				$scope.qualirtStatsRecords.push(tmpArray);
+			}
+		
+		};
 		
 		function sendHttpRequest (service){
 			var defer = $q.defer();
@@ -457,6 +613,7 @@ aviateAdmin.controller("superDashboardCtrl", ['$scope', '$localStorage', '$locat
 				$scope.stores = data;
 				console.log("Store success case -- ", data);
 				postStore(data);
+				groupedBarChart();
 			});
 
 		};
