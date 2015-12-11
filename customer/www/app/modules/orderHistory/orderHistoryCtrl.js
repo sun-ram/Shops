18dd27d1185f98c80c52e7a90188d4587f98a420
kamaldlk <kamalcdsk@gmail.com>
2015-12-11 15:36:12 +0530

    order track

--- customer/www/app/modules/orderHistory/orderHistoryCtrl.js
+++ customer/www/app/modules/orderHistory/orderHistoryCtrl.js
@@ -1,31 +1,16 @@
 angular.module('aviate.controllers')
 .controller("orderHistoryCtrl",
-		['$scope', '$state', 'toastr', 'CONSTANT', '$http', 'MyListServices', 'ipCookie','$rootScope','orderHistoryServices',
-		 function($scope, $state, toastr, CONSTANT, $http, MyListServices, ipCookie,$rootScope,orderHistoryServices) {
+		['$scope', '$state', 'toastr', 'CONSTANT', '$http', 'MyListServices', 'ipCookie','$rootScope','orderHistoryServices','$mdBottomSheet',
+		 function($scope, $state, toastr, CONSTANT, $http, MyListServices, ipCookie,$rootScope,orderHistoryServices,$mdBottomSheet) {
              orderHistoryServices.getordersHistory().then(function(data){
                  $scope.data = data.salesOrderList;
                  console.log("data :: ", $scope.data );
              });
-             
-                 
-             
-     
-  
-  $scope.columns = [{
-    headerName: "",
-    fieldName: "picture"
-  }, {
-    headerName: "Lastname",
-    fieldName: "lastname"
-  }, {
-    headerName: "Firstname",
-    fieldName: "firstname"
-  }, {
-    headerName: "Email",
-    fieldName: "email"
-  }, {
-    headerName: "Birthday",
-    fieldName: "birthday"
-  }];
+            
+        $scope.showListBottomSheet = function($event) {
+          
+            
+            
+        };
  
 }]);
