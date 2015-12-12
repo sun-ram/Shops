60521da6f757c2b2f974fe3ae7fbfdefcbf7cc10
Riyazk67 <riyazkhan.m@mitosistech.com>
2015-12-12 11:21:17 +0530

    Added conformation popup for favourite.

--- customer/www/app/modules/favourite/favouriteCtrl.js
+++ customer/www/app/modules/favourite/favouriteCtrl.js
@@ -1,6 +1,6 @@
 angular.module('aviate.controllers')
-.controller("favouriteCtrl", ['$scope','$http','$stateParams','$rootScope','$state', 'FavouriteServices','MyCartServices','MyCartFactory',
-                              function($scope,$http,$stateParams, $rootScope,$state, FavouriteServices,MyCartServices,MyCartFactory) {
+.controller("favouriteCtrl", ['$scope','$http','$stateParams','$rootScope','$state', 'FavouriteServices','MyCartServices','MyCartFactory','$mdDialog',
+                              function($scope,$http,$stateParams, $rootScope,$state, FavouriteServices,MyCartServices,MyCartFactory,$mdDialog) {
 
 	$scope.addFavourite = function(){
 		$scope.favourite = {};
@@ -58,5 +58,70 @@ angular.module('aviate.controllers')
 
 		});	
 		
+	}
+	
+	$rootScope.conformFavourite = function(ev){
+		if($rootScope.myCart.cartItem.length == 0){
+			$scope.favouriteToCheckout(ev);
+		}else{
+		$rootScope.favouritePopup(ev);
+		}
+		
+	}
+	
+	$rootScope.favouritePopup = function(ev){
+		$mdDialog.show({
+			templateUrl: 'app/modules/favourite/favouriteCheckout.html',
+			parent: angular.element(document.body),
+			targetEvent: ev,
+			clickOutsideToClose:true,
+			controller: function($scope, AuthServices, toastr, CONSTANT,ipCookie){
+				$scope.title = 'SIGN IN';
+				
+				$scope.favouriteToCheckout = function(){
+					$scope.favourite ={};
+					$scope.favourite.customerId = $rootScope.user.userId;
+					$scope.favourite.merchantId = $rootScope.store.merchant.merchantId;
+					$scope.favourite.storeId = $rootScope.store.storeId;
+					$scope.favourite.favouriteId = ev;
+					FavouriteServices.favouriteToCheckout($scope.favourite).then(function(data){
+						MyCartServices.getCartList({"customer" : {"customerId" : $rootScope.user.userId},"store" : {"storeId" : $rootScope.store.storeId}}).then(function(data){
+							MyCartFactory.myCartTotalPriceCalculation();
+							$state.go('app.checkout');
+							$scope.cancel();
+						});
+
+					});	
+					
+				}
+				
+				$scope.addFavouriteToCart = function(){
+
+					$scope.favourite ={};
+					$scope.favourite.customerId = $rootScope.user.userId;
+					$scope.favourite.merchantId = $rootScope.store.merchant.merchantId;
+					$scope.favourite.storeId = $rootScope.store.storeId;
+					$scope.favourite.favouriteId = ev;
+					FavouriteServices.addFavouriteToCart($scope.favourite).then(function(data){
+						MyCartServices.getCartList({"customer" : {"customerId" : $rootScope.user.userId},"store" : {"storeId" : $rootScope.store.storeId}}).then(function(data){
+							MyCartFactory.myCartTotalPriceCalculation();
+							$state.go('app.checkout');
+							$scope.cancel();
+						});
+
+					});	
+				}
+				
+				$scope.cancel = function() {
+					$mdDialog.cancel();
+				};
+
+			}
+		})
+		.then(function() {
+
+		}, function() {
+
+		});
 	}
 }]);
