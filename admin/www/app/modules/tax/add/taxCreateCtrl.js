43d88932c2e10c96fe8aaab749344294b100cc08
Suchismita <suchismita.r@mitosistech.com>
2015-12-29 15:50:57 +0530

    userid in employee and tax

--- admin/www/app/modules/tax/add/taxCreateCtrl.js
+++ admin/www/app/modules/tax/add/taxCreateCtrl.js
@@ -1,11 +1,12 @@
 angular.module('aviateAdmin.controllers')
 	.controller("taxCreateCtrl", 
-	['$scope', '$state','toastr','TaxServices','$rootScope',
-	 function($scope, $state, toastr, TaxServices, $rootScope) {
+	['$scope','$rootScope', '$state','toastr','TaxServices','$rootScope',
+	 function($scope,$rootScope, $state, toastr, TaxServices, $rootScope) {
 		$scope.addTax = function(){
 			$scope.tax.merchantVo = {
 					merchantId:$rootScope.user.merchantId
 			}
+			$scope.tax.userId=$rootScope.user.userName;
 			TaxServices.addNewTax($scope.tax).then(function(data){
 				//toastr.success(data.status);
 				$scope.tax = null;
