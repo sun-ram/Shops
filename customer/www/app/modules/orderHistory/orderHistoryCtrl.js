212b31eece71ea96d73f1911557f19c5283d25d5
rajamohamedbarook <rajamohamed.b@mitosistech.com>
2016-01-05 17:46:14 +0530

    Order history and tracking

--- customer/www/app/modules/orderHistory/orderHistoryCtrl.js
+++ customer/www/app/modules/orderHistory/orderHistoryCtrl.js
@@ -4,7 +4,8 @@ angular.module('aviate.controllers')
 		 function($scope, $state, toastr, CONSTANT, $http, MyListServices, ipCookie,$rootScope,orderHistoryServices,$mdBottomSheet,myConfig,$q) {
              orderHistoryServices.getordersHistory().then(function(data){
                  $scope.data = data.salesOrderList;
-                 console.log("data :: ", $scope.data );
+                 postSalesOrder($scope.data);
+				 console.log("data ", $scope.data );
              });
             $scope.showDetails = "";
             $scope.expand = false;
@@ -18,69 +19,27 @@ angular.module('aviate.controllers')
             
         };
 			 
-		// Order tracking 
-			 var commonNodeURL = myConfig.node_server_url;
-		$scope.trackedOrders = [];
-		$scope.trackedOrderDetails = [];
-			function sendHttpRequest (service){
-				var defer = $q.defer();
-				$http({
-						method: 'GET',
-						url: commonNodeURL+service
-					})
-					.success(function (data, status) {
-						defer.resolve(data);
-					})
-					.error(function (data, status) {
-						defer.reject(data);
-						console.log(service+" error case ", data);
-					});
-				return  defer.promise;
-			}
-
-		
 		function postSalesOrder (data){
-			console.log("User ==>",$rootScope.user);
-			/*$scope.trackedOrders = [['highlight-index','none','none','none'],['highlight-index','highlight-index','none','none'],['highlight-index','highlight-index','none','none']];*/
-			var i=0,len= ((data)?data.Books.length : 0);
+			var i=0,len= ((data)?data.length : 0);
 			for(;i<len;i++){
-				var tmpData = data.Books[i];
+				var tmpData = data[i];
 				var tmpArray = [];
-				
-				if(tmpData.DELIVERED_TIME){
+				 data[i].tracked = [];
+				if(tmpData.deliveredTime){
 					tmpArray = ['highlight-index','highlight-index','highlight-index','highlight-index'];
-					$scope.trackedOrders.push(tmpArray);
-					$scope.trackedOrderDetails.push(tmpData.NET_AMOUNT + " : "+ tmpData.ORDER_NO);
-				}else if(tmpData.DELIVERY_START_TIME || tmpData.BACKER_ASSIGNED_TIME){
+					/*$scope.trackedOrderDetails.push(tmpData.netAmount + " : "+ tmpData.orderNo);*/
+				}else if(tmpData.deliveryStartTime || tmpData.backerAssignedTime){
 					tmpArray = ['highlight-index','highlight-index','highlight-index','none'];
-					$scope.trackedOrders.push(tmpArray);
-					$scope.trackedOrderDetails.push(tmpData.NET_AMOUNT + " : "+ tmpData.ORDER_NO);
-				}else if(tmpData.PACKED_TIME){
+				}else if(tmpData.packedTime){
 					tmpArray = ['highlight-index','highlight-index','none','none'];
-					$scope.trackedOrders.push(tmpArray);
-					$scope.trackedOrderDetails.push(tmpData.NET_AMOUNT + " : "+ tmpData.ORDER_NO);
-				}else if(tmpData.PICKUP_START_TIME || tmpData.SHOPPER_ASSIGNED_TIME){
+				}else if(tmpData.pickupStartTime || tmpData.shopperAssignedTime){
 					tmpArray = ['highlight-index','none','none','none'];
-					$scope.trackedOrders.push(tmpArray);
-					$scope.trackedOrderDetails.push(tmpData.NET_AMOUNT + " : "+ tmpData.ORDER_NO);
 				}else{
 					tmpArray = ['none','none','none','none'];
-					$scope.trackedOrders.push(tmpArray);
-					$scope.trackedOrderDetails.push(tmpData.NET_AMOUNT + " : "+ tmpData.ORDER_NO);
 				}
+				$scope.data[i].tracked = angular.copy(tmpArray);
 				
-			}
-			console.info("$scope.trackedOrders---------------->",$scope.trackedOrders);
-			
-		};
-		
-		$scope.proceedSalesOrder = function (callback) {
-			sendHttpRequest('salesOrder').then(function (data) {
-				data.Books = _.reject(data.Books, function(book){ return book.CUSTOMER_ID != $rootScope.user.userId;});
-				console.info("salesOrder Received ", data);
-				postSalesOrder(data);
-			});
-		};
-		$scope.proceedSalesOrder();
+			}			
+		};		
  
 }]);
