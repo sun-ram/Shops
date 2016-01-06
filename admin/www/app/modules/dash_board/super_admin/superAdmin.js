64ff16b4f9db7aa048acec01088f31925f350402
rajamohamedbarook <rajamohamed.b@mitosistech.com>
2016-01-06 17:42:01 +0530

    Dashboard Updates

--- admin/www/app/modules/dash_board/super_admin/superAdmin.js
+++ admin/www/app/modules/dash_board/super_admin/superAdmin.js
@@ -636,125 +636,326 @@ aviateAdmin.controller("superDashboardCtrl", ['$scope', '$localStorage', '$locat
 		}
 		
 		function groupedBarChart (){
-			var data = [];
-			var i=0;len=$scope.qualirtStatsRecords.length;
-			var tmpGroupObj = {};
-			for(;i<len;i++){
-				tmpGroupObj = {};
-				tmpGroupObj.date = $scope.merchants.Books[i].NAME;
-				tmpGroupObj.Good = $scope.qualirtStatsRecords[i][0];
-				tmpGroupObj.Normal = $scope.qualirtStatsRecords[i][1];
-				tmpGroupObj.Bad = $scope.qualirtStatsRecords[i][2];
-				if(tmpGroupObj.Good >0 || tmpGroupObj.Normal > 0 || tmpGroupObj.Bad > 0){
-					data.push(angular.copy(tmpGroupObj));
+			console.log("$scope.qualirtStatsRecords",$scope.qualirtStatsRecords);
+			function dashboard(id, fData){
+				var barColor = 'steelblue';
+				function segColor(c){ return {Bad:"red", Fair:"#e08214",Good:"#41ab5d"}[c]; }
+
+				// compute total for each state.
+				fData.forEach(function(d){d.total=d.freq.Bad+d.freq.Fair+d.freq.Good;});
+
+				// function to handle histogram.
+				function histoGram(fD){
+					var hG={},    hGDim = {t: 60, r: 0, b: 30, l: 0};
+					hGDim.w =  500 - hGDim.l - hGDim.r, 
+					hGDim.h = 300 - hGDim.t - hGDim.b;
+
+					//create svg for histogram.
+					var hGsvg = d3.select(id).append("svg")
+						.attr("width", hGDim.w + hGDim.l + hGDim.r)
+						.attr("height", hGDim.h + hGDim.t + hGDim.b).append("g")
+						.attr("transform", "translate(" + hGDim.l + "," + hGDim.t + ")");
+
+					// create function for x-axis mapping.
+					var x = d3.scale.ordinal().rangeRoundBands([0, hGDim.w], 0.1)
+							.domain(fD.map(function(d) { return d[0]; }));
+
+					// Add x-axis to the histogram svg.
+					hGsvg.append("g").attr("class", "x axis")
+						.attr("transform", "translate(0," + hGDim.h + ")")
+						.call(d3.svg.axis().scale(x).orient("bottom"));
+
+					// Create function for y-axis map.
+					var y = d3.scale.linear().range([hGDim.h, 0])
+							.domain([0, d3.max(fD, function(d) { return d[1]; })]);
+
+					// Create bars for histogram to contain rectangles and freq labels.
+					var bars = hGsvg.selectAll(".bar").data(fD).enter()
+							.append("g").attr("class", "bar");
+
+					//create the rectangles.
+					bars.append("rect")
+						.attr("x", function(d) { return x(d[0]); })
+						.attr("y", function(d) { return y(d[1]); })
+						.attr("width", x.rangeBand())
+						.attr("height", function(d) { return hGDim.h - y(d[1]); })
+						.attr('fill',barColor)
+						.on("mouseover",mouseover)// mouseover is defined below.
+						.on("mouseout",mouseout);// mouseout is defined below.
+
+					//Create the frequency labels above the rectangles.
+					bars.append("text").text(function(d){ return d3.format(",")(d[1])})
+						.attr("x", function(d) { return x(d[0])+x.rangeBand()/2; })
+						.attr("y", function(d) { return y(d[1])-5; })
+						.attr("text-anchor", "middle");
+
+					function mouseover(d){  // utility function to be called on mouseover.
+						// filter for selected state.
+						var st = fData.filter(function(s){ return s.State == d[0];})[0],
+							nD = d3.keys(st.freq).map(function(s){ return {type:s, freq:st.freq[s]};});
+
+						// call update functions of pie-chart and legend.    
+						pC.update(nD);
+						leg.update(nD);
+					}
+
+					function mouseout(d){    // utility function to be called on mouseout.
+						// reset the pie-chart and legend.    
+						pC.update(tF);
+						leg.update(tF);
+					}
+
+					// create function to update the bars. This will be used by pie-chart.
+					hG.update = function(nD, color){
+						// update the domain of the y-axis map to reflect change in frequencies.
+						y.domain([0, d3.max(nD, function(d) { return d[1]; })]);
+
+						// Attach the new data to the bars.
+						var bars = hGsvg.selectAll(".bar").data(nD);
+
+						// transition the height and color of rectangles.
+						bars.select("rect").transition().duration(500)
+							.attr("y", function(d) {return y(d[1]); })
+							.attr("height", function(d) { return hGDim.h - y(d[1]); })
+							.attr("fill", color);
+
+						// transition the frequency labels location and change value.
+						bars.select("text").transition().duration(500)
+							.text(function(d){ return d3.format(",")(d[1])})
+							.attr("y", function(d) {return y(d[1])-5; });            
+					}        
+					return hG;
 				}
+
+				// function to handle pieChart.
+				function pieChart(pD){
+					var pC ={},    pieDim ={w:200, h: 200};
+					pieDim.r = Math.min(pieDim.w, pieDim.h) / 2;
+
+					// create svg for pie chart.
+					var piesvg = d3.select(id).append("svg")
+						.attr("width", pieDim.w).attr("height", pieDim.h).append("g")
+						.attr("transform", "translate("+pieDim.w/2+","+pieDim.h/2+")");
+
+					// create function to draw the arcs of the pie slices.
+					var arc = d3.svg.arc().outerRadius(pieDim.r - 10).innerRadius(0);
+
+					// create a function to compute the pie slice angles.
+					var pie = d3.layout.pie().sort(null).value(function(d) { return d.freq; });
+
+					// Draw the pie slices.
+					piesvg.selectAll("path").data(pie(pD)).enter().append("path").attr("d", arc)
+						.each(function(d) { this._current = d; })
+						.style("fill", function(d) { return segColor(d.data.type); })
+						.on("mouseover",mouseover).on("mouseout",mouseout);
+
+					// create function to update pie-chart. This will be used by histogram.
+					pC.update = function(nD){
+						piesvg.selectAll("path").data(pie(nD)).transition().duration(500)
+							.attrTween("d", arcTween);
+					}        
+					// Utility function to be called on mouseover a pie slice.
+					function mouseover(d){
+						// call the update function of histogram with new data.
+						hG.update(fData.map(function(v){ 
+							return [v.State,v.freq[d.data.type]];}),segColor(d.data.type));
+					}
+					//Utility function to be called on mouseout a pie slice.
+					function mouseout(d){
+						// call the update function of histogram with all data.
+						hG.update(fData.map(function(v){
+							return [v.State,v.total];}), barColor);
+					}
+					// Animating the pie-slice requiring a custom function which specifies
+					// how the intermediate paths should be drawn.
+					function arcTween(a) {
+						var i = d3.interpolate(this._current, a);
+						this._current = i(0);
+						return function(t) { return arc(i(t));    };
+					}    
+					return pC;
+				}
+
+				// function to handle legend.
+				function legend(lD){
+					var leg = {};
+
+					// create table for legend.
+					var legend = d3.select(id).append("table").attr('class','legend');
+
+					// create one row per segment.
+					var tr = legend.append("tbody").selectAll("tr").data(lD).enter().append("tr");
+
+					// create the first column for each segment.
+					tr.append("td").append("svg").attr("width", '16').attr("height", '16').append("rect")
+						.attr("width", '16').attr("height", '16')
+						.attr("fill",function(d){ return segColor(d.type); });
+
+					// create the second column for each segment.
+					tr.append("td").text(function(d){ return d.type;});
+
+					// create the third column for each segment.
+					tr.append("td").attr("class",'legendFreq')
+						.text(function(d){ return d3.format(",")(d.freq);});
+
+					// create the fourth column for each segment.
+					tr.append("td").attr("class",'legendPerc')
+						.text(function(d){ return getLegend(d,lD);});
+
+					// Utility function to be used to update the legend.
+					leg.update = function(nD){
+						// update the data attached to the row elements.
+						var l = legend.select("tbody").selectAll("tr").data(nD);
+
+						// update the frequencies.
+						l.select(".legendFreq").text(function(d){ return d3.format(",")(d.freq);});
+
+						// update the percentage column.
+						l.select(".legendPerc").text(function(d){ return getLegend(d,nD);});        
+					}
+
+					function getLegend(d,aD){ // Utility function to compute percentage.
+						return d3.format("%")(d.freq/d3.sum(aD.map(function(v){ return v.freq; })));
+					}
+
+					return leg;
+				}
+
+				// calculate total frequency by segment for all state.
+				var tF = ['Bad','Fair','Good'].map(function(d){ 
+					return {type:d, freq: d3.sum(fData.map(function(t){ return t.freq[d];}))}; 
+				});    
+
+				// calculate total frequency by state for all segment.
+				var sF = fData.map(function(d){return [d.State,d.total];});
+
+				var hG = histoGram(sF), // create the histogram.
+					pC = pieChart(tF), // create the pie-chart.
+					leg= legend(tF);  // create the legend.
 			}
-			// Set our margins
-			var margin = {
-				top: 20,
-				right: 20,
-				bottom: 30,
-				left: 60
-			},
-			width = 700 - margin.left - margin.right,
-			height = 350 - margin.top - margin.bottom;
-
-			var x0 = d3.scale.ordinal()
-				.rangeRoundBands([0, width], .1);
-
-			var x1 = d3.scale.ordinal();
-
-			var y = d3.scale.linear()
-				.range([height, 0]);
-
-			var color = d3.scale.ordinal()
-				.range(["#4eaf4c", "#ffff00", "#ff0000"]);
-
-			var xAxis = d3.svg.axis()
-				.scale(x0)
-				.orient("bottom");
-
-			var yAxis = d3.svg.axis()
-				.scale(y)
-				.orient("left")
-				.tickFormat(d3.format(".2s"));
-
-			
-			// Add our chart to the #chart div
-			var svg = d3.select("#grouperBarChart").append("svg")
-				.attr("width", width + margin.left + margin.right)
-				.attr("height", height + margin.top + margin.bottom)
-				.append("g")
-				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-		
-			//array of keys except date
-			var keys = d3.keys(data[0]).filter(function(key) { return key !== "date"; });
-
-			data.forEach(function(d) {
-				d.key = keys.map(function(name) { 
-				  console.log("printing the name: "+name+ " "+d[name])
-				  return {name: name, value: +d[name]}; });
-			});
-
-			x0.domain(data.map(function(d) { return d.date; }));
-			x1.domain(keys).rangeRoundBands([0, x0.rangeBand()]);
-			y.domain([0, d3.max(data, function(d) { return d3.max(d.key, function(d) { return d.value; }); })]);
-
-			  svg.append("g")
-				  .attr("class", "x axis")
-				  .attr("transform", "translate(0," + height + ")")
-				  .call(xAxis);
-
-			  svg.append("g")
-				  .attr("class", "y axis")
-				  .call(yAxis)
-				.append("text")
-				  .attr("transform", "rotate(-90)")
-				  .attr("y", 6)
-				  .attr("dy", ".71em")
-				  .style("text-anchor", "end")
-				  .text("count");
-
-			  var date = svg.selectAll(".date")
-				  .data(data)
-				  .enter().append("g")
-				  .attr("class", "g")
-				  .attr("transform", function(d) { return "translate(" + x0(d.date) + ",0)"; });
-
-			  date.selectAll("rect")
-				  .data(function(d) { return d.key; })
-				  .enter().append("rect")
-				  .attr("width", x1.rangeBand())
-				  .attr("x", function(d) { return x1(d.name); })
-				  .attr("y", function(d) { return y(d.value); })
-				  .attr("height", function(d) { return height - y(d.value); })
-				  .style("fill", function(d) { return color(d.name); });
-
-			//for showing the legend
-			var legend = svg.selectAll(".legend")
-				.data(color.domain().slice().reverse())
-				.enter().append("g")
-				.attr("class", "legend")
-				.attr("transform", function (d, i) {
-				return "translate(0," + i * 20 + ")";
-			});
-
-			legend.append("rect")
-				.attr("x", width - 18)
-				.attr("width", 18)
-				.attr("height", 18)
-				.style("fill", color);
-
-			legend.append("text")
-				.attr("x", width - 24)
-				.attr("y", 9)
-				.attr("dy", ".35em")
-				.style("text-anchor", "end")
-				.text(function (d) {
-				return d;
-			});
+			dashboard('#grouperBarChart',$scope.qualirtStatsRecords);
 		};
+		
+//		function groupedBarChart (){
+//			var data = [];
+//			var i=0;len=$scope.qualirtStatsRecords.length;
+//			var tmpGroupObj = {};
+//			for(;i<len;i++){
+//				tmpGroupObj = {};
+//				tmpGroupObj.date = $scope.merchants.Books[i].NAME;
+//				tmpGroupObj.Good = $scope.qualirtStatsRecords[i][0];
+//				tmpGroupObj.Normal = $scope.qualirtStatsRecords[i][1];
+//				tmpGroupObj.Bad = $scope.qualirtStatsRecords[i][2];
+//				if(tmpGroupObj.Good >0 || tmpGroupObj.Normal > 0 || tmpGroupObj.Bad > 0){
+//					data.push(angular.copy(tmpGroupObj));
+//				}
+//			}
+//			// Set our margins
+//			var margin = {
+//				top: 20,
+//				right: 20,
+//				bottom: 30,
+//				left: 60
+//			},
+//			width = 700 - margin.left - margin.right,
+//			height = 350 - margin.top - margin.bottom;
+//
+//			var x0 = d3.scale.ordinal()
+//				.rangeRoundBands([0, width], .1);
+//
+//			var x1 = d3.scale.ordinal();
+//
+//			var y = d3.scale.linear()
+//				.range([height, 0]);
+//
+//			var color = d3.scale.ordinal()
+//				.range(["#4eaf4c", "#ffff00", "#ff0000"]);
+//
+//			var xAxis = d3.svg.axis()
+//				.scale(x0)
+//				.orient("bottom");
+//
+//			var yAxis = d3.svg.axis()
+//				.scale(y)
+//				.orient("left")
+//				.tickFormat(d3.format(".2s"));
+//
+//			
+//			// Add our chart to the #chart div
+//			var svg = d3.select("#grouperBarChart").append("svg")
+//				.attr("width", width + margin.left + margin.right)
+//				.attr("height", height + margin.top + margin.bottom)
+//				.append("g")
+//				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//
+//		
+//			//array of keys except date
+//			var keys = d3.keys(data[0]).filter(function(key) { return key !== "date"; });
+//
+//			data.forEach(function(d) {
+//				d.key = keys.map(function(name) { 
+//				  console.log("printing the name: "+name+ " "+d[name])
+//				  return {name: name, value: +d[name]}; });
+//			});
+//
+//			x0.domain(data.map(function(d) { return d.date; }));
+//			x1.domain(keys).rangeRoundBands([0, x0.rangeBand()]);
+//			y.domain([0, d3.max(data, function(d) { return d3.max(d.key, function(d) { return d.value; }); })]);
+//
+//			  svg.append("g")
+//				  .attr("class", "x axis")
+//				  .attr("transform", "translate(0," + height + ")")
+//				  .call(xAxis);
+//
+//			  svg.append("g")
+//				  .attr("class", "y axis")
+//				  .call(yAxis)
+//				.append("text")
+//				  .attr("transform", "rotate(-90)")
+//				  .attr("y", 6)
+//				  .attr("dy", ".71em")
+//				  .style("text-anchor", "end")
+//				  .text("count");
+//
+//			  var date = svg.selectAll(".date")
+//				  .data(data)
+//				  .enter().append("g")
+//				  .attr("class", "g")
+//				  .attr("transform", function(d) { return "translate(" + x0(d.date) + ",0)"; });
+//
+//			  date.selectAll("rect")
+//				  .data(function(d) { return d.key; })
+//				  .enter().append("rect")
+//				  .attr("width", x1.rangeBand())
+//				  .attr("x", function(d) { return x1(d.name); })
+//				  .attr("y", function(d) { return y(d.value); })
+//				  .attr("height", function(d) { return height - y(d.value); })
+//				  .style("fill", function(d) { return color(d.name); });
+//
+//			//for showing the legend
+//			var legend = svg.selectAll(".legend")
+//				.data(color.domain().slice().reverse())
+//				.enter().append("g")
+//				.attr("class", "legend")
+//				.attr("transform", function (d, i) {
+//				return "translate(0," + i * 20 + ")";
+//			});
+//
+//			legend.append("rect")
+//				.attr("x", width - 18)
+//				.attr("width", 18)
+//				.attr("height", 18)
+//				.style("fill", color);
+//
+//			legend.append("text")
+//				.attr("x", width - 24)
+//				.attr("y", 9)
+//				.attr("dy", ".35em")
+//				.style("text-anchor", "end")
+//				.text(function (d) {
+//				return d;
+//			});
+//		};
 		
 		function getQualityStats (times){
 			$scope.qualirtStatsRecords = [];
@@ -767,7 +968,7 @@ aviateAdmin.controller("superDashboardCtrl", ['$scope', '$localStorage', '$locat
 				$timeout(getQualityStats(1),3000);
 			}
 			for(;($scope.merchants.Books && i<$scope.merchants.Books.length);i++){
-				tmpArray = [];
+				tmpArray = {};
 				red=green=yellow=0;
 				var j=0,solen = $scope.salesOrders.Books.length;
 				for(;j<solen;j++){
@@ -783,12 +984,20 @@ aviateAdmin.controller("superDashboardCtrl", ['$scope', '$localStorage', '$locat
 						}
 					}
 				}
-				tmpArray.push(green);
-				tmpArray.push(yellow);
-				tmpArray.push(red);	
-				/*console.log("tmpArray =",tmpArray);*/
+				tmpArray.State = $scope.merchants.Books[i].NAME;
+				tmpArray.freq = {};
+				tmpArray.freq.Bad = red;
+				tmpArray.freq.Fair = yellow;
+				tmpArray.freq.Good = red;
 				$scope.qualirtStatsRecords.push(tmpArray);
 			}
+			/*$scope.qualirtStatsRecords=[
+				{State:'AL',freq:{Bad:4786, Fair:1319, Good:249}}
+				,{State:'AZ',freq:{Bad:1101, Fair:412, Good:674}}
+				,{State:'CT',freq:{Bad:932, Fair:2149, Good:418}}
+				,{State:'DE',freq:{Bad:832, Fair:1152, Good:1862}}
+				
+				];*/
 			console.log("$scope.qualirtStatsRecords",$scope.qualirtStatsRecords);
 			groupedBarChart();
 			
